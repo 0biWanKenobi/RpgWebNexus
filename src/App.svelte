693dba267d1fc624615e0569b7b9289d0624d470
ObiWanKenobi <metal.rulesall@gmail.com>
2026-04-11 15:13:56 +0200

    wip: send g token to Obsidian

--- src/App.svelte
+++ src/App.svelte
@@ -58,6 +58,9 @@
 
     {#if tokenDisplay.verified}
       <TokenPanel tokenResult={tokenDisplay.tokenResult} />
+      <a href={`obsidian://rpg_nexus_configuration/action?setup_token&value=${tokenDisplay.tokenResult.google_access_token}`}>
+        Connect Obsidian
+      </a>
     {:else if tokenDisplay.verified === false}
       <p>WARNING: received response may have been hijacked!</p>
     {/if}
